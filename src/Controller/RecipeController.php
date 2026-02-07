dfaa89c55c1e94339f7c055d84406d6ce1aa7026
thibaultfradet <fradet.thibault23@gmail.com>
2026-02-07 10:44:03 +0100

    feat(recipe): add my recipes view and recipe editing

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Recipe;
 use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,17 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/recipe')]
 final class RecipeController extends AbstractController
 {
+    #[Route('/my', name: 'app_recipe_my')]
+    public function myRecipes(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('recipe/my.html.twig', [
+            'recipes' => $recipeRepository->findBy(
+                ['author' => $this->getUser()],
+                ['createdAt' => 'DESC']
+            ),
+        ]);
+    }
+
     #[Route('/new', name: 'app_recipe_new')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -29,11 +41,44 @@ final class RecipeController extends AbstractController
             $entityManager->persist($recipe);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_recipe_show', ['id' => $recipe->getId()]);
         }
 
         return $this->render('recipe/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_recipe_show', requirements: ['id' => '\d+'])]
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_recipe_edit', requirements: ['id' => '\d+'])]
+    public function edit(Recipe $recipe, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Only the author can edit their recipe
+        if ($recipe->getAuthor() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Vous ne pouvez modifier que vos propres recettes.');
+        }
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe->setUpdatedAt(new \DateTimeImmutable());
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_recipe_show', ['id' => $recipe->getId()]);
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'form' => $form,
+            'recipe' => $recipe,
         ]);
     }
 }
