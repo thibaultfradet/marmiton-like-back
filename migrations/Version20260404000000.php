f8e84bee1e782d694629bf0fd671a3cd6e533b36
thibaultfradet <fradet.thibault23@gmail.com>
2026-04-04 15:21:56 +0200

    chore: migrate database from PostgreSQL to MySQL 8.0 with phpMyAdmin

--- migrations/Version20260404000000.php
+++ migrations/Version20260404000000.php
@@ -17,23 +17,21 @@ final class Version20260404000000 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE reset_password_request (
-            id SERIAL NOT NULL,
+            id INT AUTO_INCREMENT NOT NULL,
             user_id INT NOT NULL,
             selector VARCHAR(20) NOT NULL,
             hashed_token VARCHAR(100) NOT NULL,
-            requested_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-            expires_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+            requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\',
+            expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\',
+            INDEX IDX_7CE748AA76ED395 (user_id),
             PRIMARY KEY(id)
-        )');
-        $this->addSql('CREATE INDEX IDX_7CE748AA76ED395 ON reset_password_request (user_id)');
-        $this->addSql('COMMENT ON COLUMN reset_password_request.requested_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN reset_password_request.expires_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        ) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE reset_password_request DROP CONSTRAINT FK_7CE748AA76ED395');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('DROP TABLE reset_password_request');
     }
 }
